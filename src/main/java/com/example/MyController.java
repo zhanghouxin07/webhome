cb7fbf3e964641da7990d0a01ecd128b52829585
houxin <894112607@qq.com>
2024-07-13 23:31:20 +0800

    增加上传文件的前端页面后台接口

--- src/main/java/com/example/MyController.java
+++ src/main/java/com/example/MyController.java
@@ -1,9 +1,10 @@
 package com.example;
 
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,5 +26,24 @@ public class MyController {
         fis.read(bytes);
         fis.close();
         return bytes;
+    }
+
+    @Value("${file.upload.directory}")
+    private String uploadDirectory;
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        if (file.isEmpty()) {
+            return "上传文件为空";
+        }
+
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+        // 创建文件对象
+        File destFile = new File(uploadDirectory + fileName);
+        // 保存文件
+        FileUtils.copyInputStreamToFile(file.getInputStream(), destFile);
+
+        return "上传成功";
     }
 }
